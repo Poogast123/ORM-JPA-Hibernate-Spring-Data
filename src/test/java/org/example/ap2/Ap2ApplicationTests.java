71e00a86e4066e11ad3a8898f08e75fb64176058
kharifi oussama <oussamakharifi2003@gmail.com>
2025-03-22 13:18:30 +0000

    v1

---
+++ src/test/java/org/example/ap2/Ap2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ap2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ap2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
